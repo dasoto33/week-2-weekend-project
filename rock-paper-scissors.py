238bdbaa4608ffc1660a51871a6c5890d7698213
Gouldy-C <mail4clg@gmail.com>
2023-06-23 11:49:57 -0700

    christian first commit

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -11,7 +11,7 @@ ties = 0
 
 def main():
     while True:
-       choice =  get_input()
+    choice =  get_input()
 
 
     
@@ -30,24 +30,16 @@ def get_input():
 
 
 def winner_loser(human_choice, comp_choice)
-    if (
-        (human_choice == 'Rock' and comp_choice == 'Paper', 'Spock') or
-        (human_choice == 'Paper' and comp_choice == 'Scisors', 'Lizard') or
-        (human_choice == 'Scisors' and comp_choice == 'Rock', 'Spock') or
-        (human_choice == 'Spock' and comp_choice == 'Lizard', 'Scisors') or
-        (human_choice == 'Lizard' and comp_choice == 'Scisors', 'Rock')
-    ):
+    if ((human_choice == 'Rock' and comp_choice in ['Paper', 'Spock']) or
+        (human_choice == 'Paper' and comp_choice in ['Scisors', 'Lizard']) or
+        (human_choice == 'Scisors' and comp_choice in ['Rock', 'Spock']) or
+        (human_choice == 'Spock' and comp_choice in ['Lizard', 'Scisors']) or
+        (human_choice == 'Lizard' and comp_choice in ['Scisors', 'Rock'])):
         return 'You Lose'
     elif human_choice == comp_choice:
         return "Tie"
     else:
         return 'You Win'
-
-
-options = ['Rock', 'Paper', 'Scisors', 'Spock', 'Lizard']
-
-
-
 
 def close_game():
     print(f'Human Score; {human_score} | Computer Score; {comp_score} | Ties; {ties}')
